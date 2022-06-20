917d528ddfad1aaeb804a2e6e0f44a7dc803e989
Jafarammer <jafalkaff96@gmail.com>
2022-06-20 17:09:05 +0700

    add function edit and delete in users

--- user.js
+++ user.js
@@ -44,6 +44,51 @@ app.post("/users/add", (req, res) => {
   );
 });
 
+// Edit
+app.patch("/users/edit", (req, res) => {
+  const { name, email, password, id } = req.body;
+  db.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      res.status(400).send("There is an error");
+    } else {
+      if (results.rowCount > 0) {
+        let nameInput = name || results?.rows[0].name;
+        let emailInput = email || results?.rows[0].email;
+        let passInput = password || results?.rows[0].password;
+        const hash = crypto
+          .createHmac("sha256", passInput)
+          .update("very very important")
+          .digest("hex");
+        db.query(
+          `UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4`,
+          [nameInput, emailInput, hash, id],
+          (error, result) => {
+            if (error) {
+              res.status(400).send("Invalid input");
+            } else {
+              res.send("Data added successfully");
+            }
+          }
+        );
+      } else {
+        res.send("Data not found!!!");
+      }
+    }
+  });
+});
+
+// DELETE
+app.delete("/users/delete", (req, res) => {
+  const { id } = req.body;
+  db.query("DELETE FROM users WHERE id = $1", [id], (err, result) => {
+    if (err) {
+      return res.status(500).send("Internal server error");
+    } else {
+      return res.status(200).send("Data deleted successfully!");
+    }
+  });
+});
+
 // Bottom
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

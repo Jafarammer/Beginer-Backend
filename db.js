f1e8cb15520aebd9ffe8f135858d0c922a6c544e
Jafarammer <jafalkaff96@gmail.com>
2022-06-21 11:26:26 +0700

    add file env

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const Pool = require("pg").Pool;
+const dotenv = require("dotenv");
+dotenv.config();
 const connection = new Pool({
-  user: "jafarammer",
-  host: "localhost",
-  database: "sweet_food",
-  password: "admin",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 module.exports = connection;
